e91f25d1ffd9b7249abfcc3ff82761d785cefbe7
Heaven <technicalheaven01@gmail.com>
2024-07-02 00:20:29 +0530

    setup routes and middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import express, { Request, Response } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
+import userRoutes from './modules/user/routes';
+import { responseHandler } from './middlewares/responseHandler';
+import { errorHandler } from './middlewares/errorHandler';
+const app:Application = express();
+const port:number = Number(process.env.PORT) || 3000;
 
-const app = express();
-const port = process.env.PORT || 3000;
+// Use the response handler
+app.use(responseHandler);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
+app.use('/users', userRoutes);
+
+// Use the error handler
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);

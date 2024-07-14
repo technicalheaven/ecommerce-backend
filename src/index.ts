5a845bdd25cc5f8661ef0cc24fa8a1be6282d7c2
Heaven <technicalheaven01@gmail.com>
2024-07-14 21:32:11 +0530

    setup bodyparser and cors

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import userRoutes from './modules/user/routes';
 import { responseHandler } from './middlewares/responseHandler';
 import { errorHandler } from './middlewares/errorHandler';
 import logger from './libs/logger/logger';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
 const app:Application = express();
 const port:number = Number(process.env.PORT) || 3000;
 
 // Use the response handler
 app.use(responseHandler);
+
+// Setup bodyparser
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors()); // Enable All CORS Requests
 
 app.use('/users', userRoutes);
 
